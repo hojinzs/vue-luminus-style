81ff3f1802d55411b1aeee56f872b69b33644296
Steve Lee <hojinzs@gmail.com>
2020-02-21 02:09:43 +0900

    패키징 수정

--- luminus/components.js
+++ luminus/components.js
@@ -3,7 +3,11 @@
 import lumiCaroucel from './components/lumi-carousel.vue'
 import lumiCaroucelSlide from './components/lumi-carousel-slide.vue'
 
+import Vue2TouchEvents from 'vue2-touch-events'
+
 const install = Vue => {
+    Vue.use(Vue2TouchEvents)
+
     Vue.component('lumiCaroucel', lumiCaroucel);
     Vue.component('lumiCaroucelSlide', lumiCaroucelSlide);
 }
